8a8a757acc182af01713bffebed94d4601f2971f
Magnani Fabiola Zaheza Ruzibiza <magnanifabiola@Magnanis-Air-6.attlocal.net>
2024-12-31 12:14:02 -0500

    Initial commit

---
+++ Js.js
@@ -0,0 +1,10 @@
+
+function togglePanel(panelId) {
+    var panel = document.getElementById(panelId);
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  }
+  
